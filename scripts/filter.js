2091ee5d518b3526a047a34eb37432bc22ebd064
cole peden <cole.peden@outlook.com>
2025-04-24 12:03:16 -0400

    init commit

---
+++ scripts/filter.js
@@ -0,0 +1,21 @@
+const a = JSON.parse(process.argv[2])
+const b = JSON.parse(process.argv[3])
+let c = []
+
+let list = [ "org", "net", "com", "cafe" ]
+
+for (const [addr, props] of Object.entries(a)) {
+	const { id, mon } = props
+	const obj = {id: id.toString(), class: b[addr], mon: mon === "DP-2" ? 0 : 1 }
+
+	// Destroy the NSID.
+	if (obj.class?.includes(".")) {
+		for (const i of list) {
+			if (!obj.class.startsWith(i)) continue;
+			obj.class = obj.class.split(".").at(-1)
+		}
+	}
+	c.push(obj)
+}
+
+console.log(JSON.stringify(c.sort((a, b) => a.id - b.id)))
